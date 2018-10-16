cec9d382a1e873ac650ac3373acecbfd5f591da3
hareeqi <hareeqi@gmail.com>
2018-10-16 12:13:35 +0300

    before push script

--- index.native.js
+++
@@ -1,5 +0,0 @@
-const { Expo } = require('./src/libs/react-one-ui');
-const App = require('./src/demo').default;
-// eslint-disable-next-line
-__DEV__ && Expo.KeepAwake.activate();
-Expo.registerRootComponent(App);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const React = require('react');
-const { Dom, serviceWorker } = require('./libs/react-one-ui');
+const { Dom, serviceWorker, isWeb, Expo } = require('react-one-ui');
 const App = require('./demo').default;
 
-Dom.render(<App />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (isWeb) {
+    Dom.render(<App />, document.getElementById('root'));
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    serviceWorker.unregister();
 
-/// Enabling hot reloading
-if (module.hot) {
-    console.log('Connected');
-    module.hot.accept('./demo', () => {
-        const Root = require('./demo').default;
-        console.log(`%c ==========  Recived update in ${Date()}`, 'background: #222; color: #bada55');
-        Dom.render(<Root />, document.getElementById('root'));
-    });
+    /// Enabling hot reloading
+    if (module.hot) {
+        console.log('Connected');
+        module.hot.accept('./demo', () => {
+            const Root = require('./demo').default;
+            console.log(`%c ==========  Recived update in ${Date()}`, 'background: #222; color: #bada55');
+            Dom.render(<Root />, document.getElementById('root'));
+        });
+    }
+} else {
+    // eslint-disable-next-line
+    __DEV__ && Expo.KeepAwake.activate();
+    Expo.registerRootComponent(App);
 }
