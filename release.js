fb8915ca9c9eb5f3220deb1673f6316436108580
hareeqi <hareeqi@gmail.com>
2018-10-16 13:51:42 +0300

    fix package.json

--- release.js
+++ release.js
@@ -1,4 +1,3 @@
-const sleep = (time = 1) => new Promise(resolve => setTimeout(resolve, time));
 const bin_dir = `${__dirname}/node_modules/.bin/`;
 const exec = require('child_process').execSync;
 const e = c => exec(c, { shell: '/bin/bash', stdio: 'inherit' });
@@ -14,34 +13,25 @@ if (!is_clean.includes('On branch master') || !is_clean.includes('working tree c
     process.exit(1);
 }
 
-const runscript = async () => {
-    console.log('===== preparing release package.json');
-    e(`mv ./package.json ./package.dev.json`);
-    e(`mv ./package.release.json ./package.json`);
+console.log('===== preparing release package.json');
+e(`mv ./package.json ./package.dev.json`);
+e(`mv ./package.release.json ./package.json`);
 
-    console.log('===== building dist files');
-    e(`rm -rf ./dist && ${bin_dir}babel ./src/libs/react-one-ui --out-dir dist`);
+console.log('===== building dist files');
+e(`rm -rf ./dist && ${bin_dir}babel ./src/libs/react-one-ui --out-dir dist`);
 
-    console.log('===== patching version');
+console.log('===== patching version');
 
-    try {
-        e(`npm version -f --no-commit-hooks --no-git-tag-version patch`);
-        //e(`npm publish`);
-    } catch (a) {
-        console.log('=====Error occured in patching and publising');
-        console.log('=====returing dev package.json');
-        await sleep(1000);
-        e(`mv ./package.json ./package.release.json`);
-        await sleep(1000);
-        console.log('=====returing dev package.json');
-
-        e(`mv ./package.dev.json ./package.json `);
-        process.exit(1);
-    }
-
-    console.log('=====returing dev package.json');
+try {
+    e(`npm version -f --no-commit-hooks --no-git-tag-version patch`);
+    e(`npm publish`);
+} catch (a) {
     e(`mv ./package.json ./package.release.json`);
-    e(`mv ./package.dev.json ./package.json `);
-};
+    e(`mv ./package.dev.json ./package.json`);
+    e(`git reset --hard`);
+    process.exit(1);
+}
 
-runscript();
+console.log('=====returing dev package.json');
+e(`mv ./package.json ./package.release.json`);
+e(`mv ./package.dev.json ./package.json `);
